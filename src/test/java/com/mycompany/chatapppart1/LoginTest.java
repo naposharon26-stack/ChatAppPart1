2ebfa1fdd272d062e9541355aec85073124a033a
Student <Student@LabVM2049939.drlayqesjmfula54diiuzdm0zd.jnbx.internal.cloudapp.net>
2026-09-17 14:00:57 +0200

    initial commit

---
+++ src/test/java/com/mycompany/chatapppart1/LoginTest.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.chatapppart1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Student
+ */
+public class LoginTest {
+    
+    public LoginTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // Test 1: Valid username
+    @Test
+    public void testValidUsername() {
+        Login login = new Login();
+
+        assertTrue(login.checkUserName("kyl_1"));
+    }
+
+    // Test 2: Username does not contain an underscore
+    @Test
+    public void testUsernameWithoutUnderscore() {
+        Login login = new Login();
+
+        assertFalse(login.checkUserName("kyle1"));
+    }
+
+    // Test 3: Username is more than five characters
+    @Test
+    public void testUsernameTooLong() {
+        Login login = new Login();
+
+        assertFalse(login.checkUserName("kyle_123"));
+    }
+}
